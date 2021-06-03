273f962f4bdd208d254a0e07eb39c2b3211d7c09
duchm <duchm@duchms-Mac-mini.local>
2021-06-03 17:55:13 +0700

    update

--- src/renderer/layouts/landing.vue
+++ src/renderer/layouts/landing.vue
@@ -2,15 +2,18 @@
   <div>
     <Header />
     <nuxt />
+    <Footer />
   </div>
 </template>
 
 <script>
 import Header from '../components/landing/Header'
+import Footer from '../components/landing/Footer'
 
 export default {
   components: {
-    Header
+    Header,
+    Footer
   }
 }
 </script>
