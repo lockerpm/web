8cc4f30f22f48537b1a3447ae335902d7c998e3a
sonnh@cystack.net <sonnh@cystack.net>
2021-06-23 18:55:58 +0700

    update

--- src/renderer/pages/upgrade/index.vue
+++ src/renderer/pages/upgrade/index.vue
@@ -223,16 +223,29 @@
                       v-for="item in cards"
                       :key="item.id_card"
                       :label="item.id_card"
-                      class="!flex mb-4"
+                      class="!flex mb-4 items-center"
                     >
-                      <div class="flex items-center w-[200px] justify-between">
-                        <div class="text-black font-bold">{{ item.card_type }}</div>
-                        <div class="text-black flex items-center">
-                          <span class="bg-[#242424] w-[4px] h-[4px] rounded-full mr-0.5" />
-                          <span class="bg-[#242424] w-[4px] h-[4px] rounded-full mr-0.5" />
-                          <span class="bg-[#242424] w-[4px] h-[4px] rounded-full mr-0.5" />
-                          <span class="bg-[#242424] w-[4px] h-[4px] rounded-full mr-0.5" />
-                          <span>{{ item.last4 }}</span>
+                      <div class="flex items-center w-[200px]">
+                        <div class="bg-[#f5f8fa] w-10 h-10 rounded flex items-center justify-center p-1 mr-4">
+                          <img v-if="item.card_type === 'Visa'" src="~/assets/images/icons/cards/visa.svg" alt="">
+                          <img v-else-if="item.card_type === 'MasterCard'" src="~/assets/images/icons/cards/master.svg" alt="">
+                          <img v-else-if="item.card_type === 'American Express'" src="~/assets/images/icons/cards/amex.svg" alt="">
+                          <img v-else-if="item.card_type === 'Discover'" src="~/assets/images/icons/cards/discover.svg" alt="">
+                          <img v-else-if="item.card_type === 'JCB'" src="~/assets/images/icons/cards/jcb.svg" alt="">
+                          <img v-else src="~/assets/images/icons/cards/card.svg" alt="">
+                        </div>
+                        <div class="">
+                          <div class="text-black font-bold mb-2">{{ item.card_type }}</div>
+                          <div class="flex items-center justify-between">
+                            <div class="text-black-500 mr-10">{{ item.expire }}</div>
+                            <div class="text-black-500 flex items-center">
+                              <span class="bg-black-500 w-[4px] h-[4px] rounded-full mr-0.5" />
+                              <span class="bg-black-500 w-[4px] h-[4px] rounded-full mr-0.5" />
+                              <span class="bg-black-500 w-[4px] h-[4px] rounded-full mr-0.5" />
+                              <span class="bg-black-500 w-[4px] h-[4px] rounded-full mr-0.5" />
+                              <span>{{ item.last4 }}</span>
+                            </div>
+                          </div>
                         </div>
                       </div>
                     </el-radio>
@@ -417,19 +430,32 @@
               <div class="">
                 <el-radio-group v-model="selectedCard" class="w-full">
                   <el-radio
-                    v-for="item in cards"
-                    :key="item.id_card"
-                    :label="item.id_card"
-                    class="!flex mb-4"
+                      v-for="item in cards"
+                      :key="item.id_card"
+                      :label="item.id_card"
+                      class="!flex mb-4 items-center"
                   >
-                    <div class="flex items-center w-[200px] justify-between">
-                      <div class="text-black font-bold">{{ item.card_type }}</div>
-                      <div class="text-black flex items-center">
-                        <span class="bg-[#242424] w-[4px] h-[4px] rounded-full mr-0.5" />
-                        <span class="bg-[#242424] w-[4px] h-[4px] rounded-full mr-0.5" />
-                        <span class="bg-[#242424] w-[4px] h-[4px] rounded-full mr-0.5" />
-                        <span class="bg-[#242424] w-[4px] h-[4px] rounded-full mr-0.5" />
-                        <span>{{ item.last4 }}</span>
+                    <div class="flex items-center w-[200px]">
+                      <div class="bg-[#f5f8fa] w-10 h-10 rounded flex items-center justify-center p-1 mr-4">
+                        <img v-if="item.card_type === 'Visa'" src="~/assets/images/icons/cards/visa.svg" alt="">
+                        <img v-else-if="item.card_type === 'MasterCard'" src="~/assets/images/icons/cards/master.svg" alt="">
+                        <img v-else-if="item.card_type === 'American Express'" src="~/assets/images/icons/cards/amex.svg" alt="">
+                        <img v-else-if="item.card_type === 'Discover'" src="~/assets/images/icons/cards/discover.svg" alt="">
+                        <img v-else-if="item.card_type === 'JCB'" src="~/assets/images/icons/cards/jcb.svg" alt="">
+                        <img v-else src="~/assets/images/icons/cards/card.svg" alt="">
+                      </div>
+                      <div class="">
+                        <div class="text-black font-bold mb-2">{{ item.card_type }}</div>
+                        <div class="flex items-center justify-between">
+                          <div class="text-black-500 mr-10">{{ item.expire }}</div>
+                          <div class="text-black-500 flex items-center">
+                            <span class="bg-black-500 w-[4px] h-[4px] rounded-full mr-0.5" />
+                            <span class="bg-black-500 w-[4px] h-[4px] rounded-full mr-0.5" />
+                            <span class="bg-black-500 w-[4px] h-[4px] rounded-full mr-0.5" />
+                            <span class="bg-black-500 w-[4px] h-[4px] rounded-full mr-0.5" />
+                            <span>{{ item.last4 }}</span>
+                          </div>
+                        </div>
                       </div>
                     </div>
                   </el-radio>
