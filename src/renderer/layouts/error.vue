6bca00370d09bcc06f64da86926a8ab1787d6177
Dang Thanh Long <longdt@cystack.net>
2022-03-12 17:32:09 +0700

    fix plans

--- src/renderer/layouts/error.vue
+++ src/renderer/layouts/error.vue
@@ -1,12 +1,12 @@
 <template>
   <div v-if="error.statusCode === 404" class="mt-[205px] mb-28">
-    <!-- <img src="~/assets/images/landing/404.svg" class="mx-auto"> -->
+    <img src="~/assets/images/404.svg" class="mx-auto">
     <div class="text-center mx-auto mt-[50px]">
       <p class="landing-font-20 text-black-600 mb-[15px]">
-        {{ $t('landing_error.not_found') }}
+        The requested page could not be found
       </p>
       <NuxtLink :to="localeRoute({name: 'index'})" class="text-primary hover:no-underline landing-font-22 font-bold">
-        {{ $t('landing_error.back_to_home') }}
+        Back to home
       </NuxtLink>
     </div>
   </div>
@@ -14,7 +14,6 @@
 
 <script>
 export default {
-  layout: 'landing',
-  props: ['error'] // you can set a custom layout for the error page
+  layout: 'landing'
 }
 </script>
