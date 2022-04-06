c29ebc5555e51fd6c96c0656ac2bfd5b87f2472f
Dang Thanh Long <longdt@cystack.net>
2022-04-06 16:17:42 +0700

    update authenticate

--- src/renderer/pages/authenticate.vue
+++ src/renderer/pages/authenticate.vue
@@ -8,7 +8,6 @@ export default {
   layout: 'authenticate',
   data () {
     return {
-
     }
   },
   mounted () {
@@ -17,19 +16,51 @@ export default {
     })
   },
   methods: {
+    updateLoginExtension (response) {
+      console.log(response)
+      // eslint-disable-next-line no-undef
+      if (response && !response.success) { console.log(response) }
+      if (response && response.success && this.$route.query.client === 'browser') {
+        this.$store.commit('UPDATE_LOGIN_EXTENSION', true)
+      }
+    },
     loginByToken () {
       this.$store.commit('UPDATE_LOADING', true)
       setTimeout(async () => {
-        if (this.$route.query.client === 'browser') {
-          window.postMessage({ command: 'locker-authResult', token: this.$route.query.token }, 'https://locker.io')
-        }
         await this.$axios.setToken(this.$route.query.token, 'Bearer')
         await this.$cookies.set('cs_locker_token', this.$route.query.token, { path: '/', ...this.environment === '' ? { secure: true } : { secure: false } })
         this.$store.commit('UPDATE_IS_LOGGEDIN', true)
-        if (this.$route.query.return_url && isString(this.$route.query.return_url)) {
-          this.$router.replace(this.localePath({ path: this.$route.query.return_url, query: { client: this.$route.query.client } }))
+
+        // sendMessage to Locker extension
+        const lockerExtensionId = process.env.extensionId
+        if (this.$route.query.client === 'browser') {
+          // window.postMessage({ command: 'locker-authResult', token: this.$route.query.token }, 'https://locker.io')
+          const extensionToken = this.$route.query.token
+          // eslint-disable-next-line no-undef
+          chrome.runtime.sendMessage(lockerExtensionId, { command: 'cs-authResult', token: extensionToken }, response => this.updateLoginExtension(response))
         } else {
-          this.$router.replace(this.localePath({ name: 'vault', query: { client: this.$route.query.client } }))
+          try {
+            const url = `${process.env.baseApiUrl}/sso/access_token`
+            this.$axios.post(url, {
+              SERVICE_URL: '/sso',
+              SERVICE_SCOPE: 'pwdmanager',
+              CLIENT: 'browser'
+            }).then(async result => {
+              console.log(result)
+              const extensionToken = result.data ? result.data.access_token : ''
+              // eslint-disable-next-line no-undef
+              chrome.runtime.sendMessage(lockerExtensionId, { command: 'cs-authResult', token: extensionToken }, response => this.updateLoginExtension(response))
+            })
+          } catch (e) {
+            console.log(e)
+          }
+        }
+        // end sendMessage
+
+        if (this.$route.query.return_url && isString(this.$route.query.return_url)) {
+          this.$router.replace(this.localePath({ path: this.$route.query.return_url }))
+        } else {
+          this.$router.replace(this.localePath({ name: 'vault' }))
         }
         this.$store.commit('UPDATE_LOADING', false)
       }, 300)
