e7e1cf174449c9a08b3897dfdae84c774a87c9b7
Thư Phạm Thi Minh <thuphamthiminh@Thus-MacBook-Pro.local>
2022-05-11 23:22:18 +0700

    fix image process step strong password

--- src/renderer/pages/password-health-checker.vue
+++ src/renderer/pages/password-health-checker.vue
@@ -88,7 +88,7 @@
               </div>
             </div>
           </div>
-          <div >
+          <div>
             <nuxt-link
               class="landing-btn text-center"
               style="padding: 28px 48px !important; font-size: 20px !important;line-height: 23px !important;"
@@ -103,7 +103,7 @@
         {{ $t('password_health_checker.header.subtitle_second') }}
       </div>
     </section>
-    <section class="py-[120px] full-width h-auto" >
+    <section class="py-[120px] full-width h-auto">
       <div class="landing-font-38 font-bold text-center">
         {{ $t('password_health_checker.security_password.title') }}
       </div>
@@ -166,7 +166,7 @@
         {{ $t('password_health_checker.process_strong_password.title') }}
       </div>
       <div class="mt-[80px]">
-        <div class="grid grid-cols-2 gap-[80px]">
+        <div class="grid grid-cols-2 gap-[80px] items-center">
           <div>
             <el-collapse v-model="activeIndex" accordion>
               <el-collapse-item
@@ -189,7 +189,13 @@
               </el-collapse-item>
             </el-collapse>
           </div>
-          <div>
+          <div class="hidden md:block ">
+            <img
+              src="~/assets/images/landing/index/img_1.png"
+              alt=""
+            >
+          </div>
+          <div class="w-full md:hidden">
             <img src="~/assets/images/landing/password-health-checker/image-process-strong-password.png">
           </div>
         </div>
