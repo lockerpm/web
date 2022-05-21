34d2c5e6c6f7f9a727a8c8d0fc73c03923eef37c
thuptm <thuptm@cystack.net>
2022-05-21 09:06:05 +0700

    fix content password health checker

--- src/renderer/pages/password-health-checker.vue
+++ src/renderer/pages/password-health-checker.vue
@@ -176,7 +176,7 @@
                 v-for="(item, index) in $t('password_health_checker.process_strong_password.process')"
                 :key="index"
                 :name="(index+1).toString()"
-                :class="index != 8 ? 'lg:mb-8 mb-16': '' "
+                :class="index != 3 ? 'lg:mb-8 mb-16': '' "
               >
                 <template slot="title">
                   <div class="flex">
