ce9dd9c079aae2a04cd5f1101b7c2a478560d123
Trung Nguyen <trungnh@cystack.net>
2022-06-11 05:00:31 +0000

    Update Footer.vue

--- src/renderer/components/landing/Footer.vue
+++ src/renderer/components/landing/Footer.vue
@@ -61,7 +61,7 @@
                   class="text-black-600 landing-font-22 landing-transition mr-6 w-10 h-10 text-center border hover:border-green"
                   style="border-radius: 50%"
                   target="_blank"
-                  href="https://www.facebook.com/locker.io"
+                  href="https://www.facebook.com/lockerpm"
                 >
                   <i class="fab fa-facebook-f mt-2" />
                 </a>
