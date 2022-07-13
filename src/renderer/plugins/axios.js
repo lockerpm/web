3de528f46d76a565d2f6be6ba1bde99c885a6630
Dang Thanh Long <longdt@cystack.net>
2022-07-13 13:40:21 +0700

    set header for staging

--- src/renderer/plugins/axios.js
+++ src/renderer/plugins/axios.js
@@ -2,6 +2,10 @@ import https from 'https'
 
 export default function ({ store, $axios, app, isDev, redirect, route }) {
   $axios.defaults.httpsAgent = new https.Agent({ rejectUnauthorized: false })
+  if (process.env.CS_ENVIRONMENT === 'staging') {
+    $axios.setHeader('CF-Access-Client-Id', process.env.ACCESS_CLIENT_ID || '')
+    $axios.setHeader('CF-Access-Client-Secret', process.env.ACCESS_CLIENT_SECRET || '')
+  }
   $axios.interceptors.request.use(request => {
     // Get token from auth.js store
     // const token = store.state.auth.accessToken
