7e7f40a2292071260a2aee64f3cbfa1b40639806
KhoiVT <khoivt@cystack.net>
2022-10-05 17:57:11 +0700

    LOCKER-995 and LOCKER-924

--- src/renderer/pages/business/index.vue
+++ src/renderer/pages/business/index.vue
@@ -68,7 +68,9 @@
             <h3 class="landing-font-38 font-semibold mb-6">{{ item.title }}</h3>
             <p class="landing-font-16 mb-8" v-html="item.desc" />
             <div class="">
-              <a class="landing-btn" @click.prevent="goto('register-section')">Start a free trial</a>
+              <a class="landing-btn" @click.prevent="goto('register-section')">
+                {{ $t('business.welcome.start') }}
+              </a>
             </div>
           </div>
           <div class="md:w-1/2 w-full" :class="index%2===0?'order-2':'md:order-1 order-3'">
@@ -176,24 +178,31 @@
               <div class="landing-font-24 font-semibold mb-5">Business</div>
               <div class="flex justify-center gap-12">
                 <div>
-                  <span class="landing-font-28 font-semibold">$8/user</span>
+                  <span class="landing-font-28 font-semibold">{{ $t('business.plans.price1_1') }}</span>
                   <br>
-                  <span class="landing-font-16 font-semibold"> per month </span>
+                  <span class="landing-font-16 font-semibold"> {{ $t('business.plans.per_month') }} </span>
                   <br>
-                  <span class="landing-font-14 text-black-500 lowercase whitespace-nowrap">{{ $t('plan.monthly') }}</span>
+                  <span class="landing-font-14 text-black-500 lowercase whitespace-nowrap">{{ $t('business.plans.monthly_pack') }}</span>
                 </div>
                 <div>
-                  <span class="landing-font-28 font-semibold">$5/user</span>
+                  <span class="landing-font-28 font-semibold">{{ $t('business.plans.price1_2') }}</span>
                   <br>
-                  <span class="landing-font-16 font-semibold"> per month </span>
+                  <span class="landing-font-16 font-semibold"> {{ $t('business.plans.per_month') }} </span>
                   <br>
-                  <span class="landing-font-14 text-black-500 lowercase whitespace-nowrap">{{ $t('plan.annually') }}</span>
+                  <span class="landing-font-14 text-black-500 lowercase whitespace-nowrap">{{ $t('business.plans.yearly_pack') }}</span>
                 </div>
               </div>
               <nuxt-link class="landing-btn mt-8 !font-medium" :to="localePath('/business#register-section')">
                 {{ $t('business.welcome.start') }}
               </nuxt-link>
-              <p class="mt-3">or <span><a :href="`${enterpriseUrl}/admin/billing`" class="text-info">Purchase now</a></span></p>
+              <p class="mt-3">
+                {{ $t('business.plans.or') }}
+                <span>
+                  <a :href="`${enterpriseUrl}/admin/billing`" class="text-info">
+                    {{ $t('business.plans.do_it') }}
+                  </a>
+                </span>
+              </p>
             </div>
           </th>
           <p class="landing-font-16">{{ $t('business.plans.desc1') }}</p>
@@ -202,15 +211,16 @@
           <th class="h-[250px] w-1/4 h-1/4">
             <div class="px-8 py-6 font-medium h-full">
               <div class="landing-font-24 font-semibold mb-9">Enterprise</div>
-              <div class="uppercase text-black-500">
-                PRICE IS SUBJECT TO <br> CUSTOM TERMS
-              </div>
+              <div
+                class="uppercase text-black-500"
+                v-html="$t('business.plans.variable_price')"
+              />
               <div class="mt-12">
                 <button class="landing-btn !text-black !bg-[#E8E8E9] !hover:bg-[#E8E8E9] !font-medium" @click="openIntercom">
                   {{ $t('business.plans.contact_us') }}
                 </button>
               </div>
-              <p class="text-black-500 mt-3">For more information</p>
+              <p class="text-black-500 mt-3">{{ $t('business.plans.more_inf') }}</p>
             </div>
           </th>
           <p class="landing-font-16">{{ $t('business.plans.desc2') }}</p>
--- src/renderer/pages/features/save-password.vue
+++ src/renderer/pages/features/save-password.vue
@@ -1,106 +1,92 @@
 <template>
   <div>
-    <!-- Feature -->
+    <!-- Intro -->
     <section
-      class="full-width w-full h-auto md:pb-[118px] pb-20"
-      style="background-color: #f5f6f7"
+      class="full-width w-full h-auto md:pb-[56px] pb-14"
+      style="background-color: #EBF2EC"
     >
       <div class="max-w-6xl mx-auto px-6">
         <div class="w-full flex flex-wrap">
-          <div class="w-full md:mb-0 md:pt-[115px] pt-20 text-center order-1">
-            <h2 class=" w-full font-bold landing-font-38 text-black mb-[20px] max-w-[650px] mx-auto">
-              {{ $t('landing.section2.title') }}
-            </h2>
-            <img
-              src="~/assets/images/landing/index/boundary2.svg"
-              alt=""
-              class="mx-auto"
-            >
-          </div>
-          <div
-            class="w-full mt-[45px] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-x-[45px] lg:gap-x-[30px] gap-y-[45px] order-2"
-          >
-            <div
-              v-for="(item, index) in $t('landing.section2.features')"
-              :key="index"
-              class="landing-transition px-[30px] pt-11 bg-white rounded-lg hover:shadow-xl"
-            >
-              <img
-                :src="require(`~/assets/images/landing/index/${item.img}`)"
-                :alt="item.title"
-                class="h-[40px] mb-[16px]"
-              >
-              <h3 class="mb-[12px] font-bold text-black landing-font-20">
-                {{ item.title }}
-              </h3>
-              <p class="lg:max-w-[265px] mb-[40px] landing-font-14">
-                {{ item.desc }}
-              </p>
+          <div class="w-full md:mb-0 md:pt-[140px] pt-20 text-center order-1">
+            <div class=" w-full font-bold landing-font-48 text-black mb-[20px] mx-auto">
+              {{ $t('features.save_password.title') }}
             </div>
-          </div>
-          <div class="max-w-xs mx-auto sm:max-w-none order-3 mt-[60px]">
-            <div class="mb-4 sm:mb-0">
-              <nuxt-link
-                class="landing-btn bg-green w-full sm:w-auto mb-1"
-                :to="localeRoute({name: $t('landing.section2.btn.link')})"
-              >
-                {{ $t('landing.section2.btn.text') }}
-              </nuxt-link>
+            <div class=" w-full landing-font-20 text-black mb-[20px] mx-auto">
+              {{ $t('features.save_password.subtitle') }}
+            </div>
+            <div class="pt-20">
+              <img src="~/assets/images/landing/features/detail/save_password.png">
             </div>
           </div>
         </div>
       </div>
     </section>
-    <!-- Feature end -->
-    <!-- Start -->
-    <div
-      class="full-width w-full h-auto pb-[105px] md:mt-[150px] mt-24"
-      style="background-color: #f5f6f7"
-    >
-      <section class="max-w-6xl mx-auto px-6">
-        <div class="w-full flex flex-wrap">
-          <div class="w-full md:mb-0 md:pt-[115px] pt-20 text-center order-1">
-            <h2 class="w-full font-bold landing-font-38 text-black mb-[45px] max-w-[790px] mx-auto">
-              {{ $t('landing.start.title') }}
-            </h2>
+    <!-- Intro end -->
+
+    <!-- Detail start -->
+    <section class="max-w-7xl mx-auto px-6">
+      <div class="w-full pt-[60px]">
+        <div
+          v-for="(detail, index) in $t('features.save_password.details')"
+          :key="index"
+          class="w-full md:mb-0 md:pt-[40px] pt-40[px] order-1 max-w-[860px]"
+        >
+          <div class="w-full font-bold landing-font-38 text-black mb-[20px] mx-auto">
+            {{ $t(detail.detail_title) }}
           </div>
-          <div class="w-full md:w-10/12 mx-auto flex flex-wrap order-2 gap-x-[30px] gap-y-[45px] justify-center">
-            <div
-              v-for="(item, index) in $t('landing.start.options')"
-              :key="index"
-              class=" landing-transition p-12 w-full md:w-[380px] bg-white relative z-10 rounded-xl hover:shadow-xl"
-            >
-              <img
-                :src="require(`~/assets/images/landing/index/${item.img}`)"
-                :alt="item.title"
-                class="mb-[20px] h-[100px] w-auto mx-auto"
-              >
-
-              <h3 class="text-center landing-font-20 font-bold mb-[11px]">
-                {{ item.title }}
-              </h3>
-
-              <p class="text-center text-black-600 landing-font-16 mb-[40px]">
-                {{ item.desc }}
-              </p>
-
-              <div class="w-full flex">
-                <nuxt-link
-                  :class="index === 0 ? 'landing-btn' : 'landing-btn2'"
-                  class="mx-auto"
-                  :to="localePath(item.btn.link)"
-                  style="align-self: center"
-                >
-                  {{ item.btn.text }}
-                </nuxt-link>
-              </div>
-            </div>
+          <div
+            class="w-full landing-font-16 text-black mx-auto"
+            style="color: #5A6176;"
+          >
+            {{ $t(detail.detail_desc) }}
           </div>
         </div>
-      </section>
-    </div>
-    <!-- Start end -->
+      </div>
+      <div class="flex">
+        <div
+          class="w-full landing-font-16 text-black mx-auto pt-[26px] flex"
+          style="color: #5A6176;"
+        >
+          {{ $t('features.save_password.get_locker') }}
+          <p class="landing-font-16">
+            <span class="text-green">
+              <nuxt-link :to="localeRoute({name: 'download'})">
+                &nbsp;here
+              </nuxt-link>
+            </span>
+          </p>
+        </div>
+      </div>
+    </section>
+    <!-- Detail end -->
+
+    <!-- CTA start -->
+    <section
+      class="full-width bg-[#EDF4ED] h-[310px] mt-[100px]"
+      :style="{backgroundImage: `url(${require(`~/assets/images/landing/features/detail/Group.png`)}), url(${require(`~/assets/images/landing/features/detail/Group-2.png`)})`}"
+      style="background-position: left bottom, right top; background-repeat: no-repeat, no-repeat"
+    >
+      <div class="w-full text-center order-1 py-16">
+        <div class=" w-full font-bold landing-font-38 text-black mb-[20px] mx-auto">
+          {{ $t('features.common_cta.title') }}
+        </div>
+        <div class=" w-full landing-font-18 text-black mb-[20px] mx-auto">
+          {{ $t('features.common_cta.subtitle') }}
+        </div>
+        <div class="w-full">
+          <nuxt-link
+            class="mx-auto landing-btn"
+            :to="localePath('/download')"
+            style="align-self: center"
+          >
+            {{ $t('features.common_cta.button_text') }}
+          </nuxt-link>
+        </div>
+      </div>
+    </section>
+    <!--CTA end -->
   </div>
+  <!-- Start end -->
 </template>
 <script>
 export default {
