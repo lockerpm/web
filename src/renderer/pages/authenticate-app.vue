0f450acc1467344728b4b829ad0a4a9be4cb9d32
linhnt <linhnt@ai-t.vn>
2022-10-09 22:52:01 +0700

    add page call back for desktop app

---
+++ src/renderer/pages/authenticate-app.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="text-center pt-[50px]">
+    <a href="lockerApp://auth?action=login&status=done">Click here to launch the app</a>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'authenticate'
+}
+</script>
+
+<style scoped>
+
+</style>
