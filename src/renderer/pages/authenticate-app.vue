6d488d0b416c10a3d0d9c602e80cf40d3598d140
linhnt <linhnt@ai-t.vn>
2022-10-10 11:15:28 +0700

    add token to link

--- src/renderer/pages/authenticate-app.vue
+++ src/renderer/pages/authenticate-app.vue
@@ -1,12 +1,20 @@
 <template>
   <div class="text-center pt-[50px]">
-    <a href="lockerApp://auth?action=login&status=done">Click here to launch the app</a>
+    <a :href="`lockerApp://auth?action=login&status=done&token=${token}`">Click here to launch the app</a>
   </div>
 </template>
 
 <script>
 export default {
-  layout: 'authenticate'
+  layout: 'authenticate',
+  mounted () {
+    this.token = this.$route.query.token
+  },
+  data () {
+    return {
+      token: null
+    }
+  }
 }
 </script>
 
