aee3d429b244d985707b7ff0559d0cf6c78cea03
duchm <duchm@MacBook-Pro.local>
2022-11-09 12:23:16 +0700

    chore: unlock business menu in landing + limit new features pages to staging

--- src/renderer/components/landing/Header.vue
+++ src/renderer/components/landing/Header.vue
@@ -66,7 +66,7 @@
               {{ $t('landing_header.personal') }}
             </nuxt-link>
           </li>
-          <li v-if="isDevOrStaging">
+          <li>
             <nuxt-link
               class="inline-block nav-item text-black landing-transition"
               :to="localePath('/business')"
