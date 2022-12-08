28ec143935c5a2bb8932cec36f95dd3d3f1f2e91
dangvh <dangvh@cystack.net>
2022-12-08 09:51:01 +0700

    add success.svg and modify authenticate-app.vue

--- src/renderer/pages/authenticate-app.vue
+++ src/renderer/pages/authenticate-app.vue
@@ -1,84 +1,47 @@
 <template>
   <div>
     <div class="w-[420px] rounded bg-white m-auto">
-      <div class="mt-[40px] mb-[60px]">
+      <div class="mt-[120px] mb-[20px]">
         <img class="h-8 mx-auto" src="~/assets/images/logo/locker_logo.png">
       </div>
-<!--      <div class="text-center mt-[40px] mb-[20px]">-->
-<!--        {{ currentUser ? currentUser.email : '' }}-->
-<!--      </div>-->
-      <div class="text-center ">
-        <h3 class="text-[24px] mb-[20px]">Great – you're in!</h3>
-        <h5>You've successfully logged in.</h5>
-      </div>
-      <div class="text-center mt-[50px]">
-        <a :href="`lockerApp://auth?action=login&status=done&token=${token}`">Click here to launch the app</a>
-      </div>
-    </div>
-    <footer class="fixed w-full mx-auto bottom-0">
-      <div class="max-w-6xl mx-auto px-6">
-        <div class="md:flex md:items-center py-3 border-t border-black-200 landing-font-12 text-black-600">
-          <div class="flex items-center">
-            <div v-if="locale==='vi'">
-              Một sản phẩm của
+      <div class="block max-w-sm p-6 bg-white border border-[#E3E3E3] rounded dark:bg-gray-800">
+        <div class="text-center ">
+          <h3 class="text-[24px] mb-[30px] mt-[80px] font-medium">Log in successful</h3>
+          <img class="h-[60px] mx-auto" src="~/assets/images/icons/success.svg">
+          <div class="inline-block select-none">
+            <div class="mt-[40px] mb-[60px] flex items-center rounded-[21px] border border-[#E3E3E3] p-2 justify-center">
+              <img :src="user ? user.avatar: ''" class="w-[28px] h-[28px] rounded-full mr-2"/>
+              {{ user ? user.email : '' }}
             </div>
-            <div v-else>
-              A product of
-            </div>
-            <a :href="locale==='vi'?'https://cystack.net/vi':'https://cystack.net'" target="_blank">
-              <img class="h-4 ml-2 mr-3" src="~/assets/images/logo/CyStack.png" alt="CyStack">
-            </a>
           </div>
-          <div class="">
-            Copyright © {{ currentYear }} Locker.io ·  All rights reserved.
-          </div>
-          <div class="flex-grow md:text-right">
-            <nuxt-link
-              :to="localePath(`/terms`)"
-              class="mr-4 text-black-600 hover:no-underline hover:text-green"
-            >
-              {{ $t('landing_footer.terms') }}
-            </nuxt-link>
-            <nuxt-link
-              :to="localePath(`/privacy`)"
-              class="text-black-600 hover:no-underline hover:text-green"
-            >
-              {{ $t('landing_footer.policy') }}
-            </nuxt-link>
+          <div>Locker desktop app will be opened shortly.</div>
+          <div class="text-center mt-[20px] mb-[80px]">
+            <a :href="`lockerApp://auth?action=login&status=done&token=${token}`">Open the app manually</a>
           </div>
         </div>
       </div>
-    </footer>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   layout: 'authenticate',
-  mounted () {
+  async mounted () {
     this.token = this.$route.query.token
-    // this.$nextTick(() => {
-    //   this.loginByToken()
-    // })
+    await this.$axios.setToken(this.$route.query.token, 'Bearer')
+    this.$axios.$get('me').then(res => {
+      console.log(res)
+      this.user = res
+    })
   },
   data () {
     return {
-      token: null
+      token: null,
+      user: null
     }
   },
-  methods: {
-    // async loginByToken () {
-    //   await this.$axios.setToken(this.$route.query.token, 'Bearer')
-    //   await this.$cookies.set('cs_locker_token', this.$route.query.token, { path: '/', ...this.environment === '' ? { secure: true } : { secure: false } })
-    //   const isLoggedIn = this.$store.state.isLoggedIn
-    //   if (!isLoggedIn) {
-    //     this.$store.commit('UPDATE_IS_LOGGEDIN', true)
-    //   }
-    //   if (!this.currentUser.email) {
-    //     await this.$store.dispatch('LoadCurrentUser')
-    //   }
-    // }
-  }
+  methods: {}
 }
 </script>
 
