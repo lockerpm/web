b9f79c1a894a45327e764f7a5ca0c42072625472
quyetnv <quyetnv@cystack.net>
2023-01-27 14:55:15 +0700

    feat: update release note menu

--- src/renderer/components/landing/release-notes/NotionContent.vue
+++ src/renderer/components/landing/release-notes/NotionContent.vue
@@ -2,20 +2,30 @@
   <div class="w-full flex flex-wrap">
     <!-- ToC -->
     <div
-      class="w-full md:w-1/4 pr-0 md:pr-5 relative md:sticky top-0 md:top-[119px]"
+      class="w-full md:w-1/4 pr-0 md:pr-5 relative md:sticky top-0 md:top-[148px]"
       style="align-self: flex-start;"
     >
-      <div style="overflow-y: scroll; max-height: calc(100vh - 119px); padding-right: 5px">
+      <div
+        style="overflow-y: scroll; max-height: calc(100vh - 119px); padding-right: 5px"
+        v-for="(menu, index) in menus"
+        :key="index"
+      >
         <a
-          v-for="(item, index) in sections"
-          :key="index"
-          class="text-black hover:text-primary font-bold hover:no-underline"
-          @click="goto(index)"
-        >
-          <p style="font-size: 16px; line-height: 20px; margin-bottom: 1.5rem;">
-            {{ item }}
-          </p>
-        </a>
+          class="text-black-700 text-head-5 hover:text-primary font-bold hover:no-underline"
+          @click="goto(menu, 'h1')"
+        >{{ menu.title }}</a>
+        <div class="ml-6 mt-3">
+          <a
+            v-for="(item, i) in menu.sections"
+            :key="i"
+            class="text-black hover:text-primary font-bold hover:no-underline"
+            @click="goto(item)"
+          >
+            <p style="font-size: 16px; line-height: 20px; margin-bottom: 1.5rem;">
+              {{ item.title }}
+            </p>
+          </a>
+        </div>
       </div>
     </div>
     <!-- ToC end -->
@@ -38,29 +48,38 @@ export default {
   },
 
   computed: {
-    sections () {
-      const sections = []
-      if (this.blockMap[this.language]) {
-        Object.values(this.blockMap[this.language]).forEach(item => {
-          if (item.value.type === 'sub_header') {
-            sections.push(item.value.properties.title[0][0])
+    menus () {
+      const allValues = Object.values(this.blockMap[this.language]).map((item, index) => ({
+        ...item,
+        title: item.value?.properties?.title[0][0] || '',
+        index
+      }))
+      const headers = allValues.filter(item => item.value.type === 'header')
+      const sections = allValues.filter(item => item.value.type === 'sub_header')
+      return headers.map((h, index) => {
+        if (index < headers.length - 1) {
+          return {
+            title: h.title,
+            sections: sections.filter(s => s.index > h.index && s.index < headers[index + 1].index)
           }
-        })
-      }
-      return sections
+        }
+        return {
+          title: h.title,
+          sections: sections.filter(s => s.index > h.index)
+        }
+      })
     }
   },
 
   methods: {
-    goto (index) {
+    goto (item, heading = 'h2') {
       if (process.client) {
-        const sections = document.querySelectorAll('h2')
-        if (sections[index]) {
-          window.scroll(0, sections[index].offsetTop - 114)
+        const sections = document.querySelectorAll(heading)
+        for (let index = 0; index < sections.length; index++) {
+          if (sections[index].innerText === item.title) {
+            window.scroll(0, sections[index].offsetTop - 140)
+          }
         }
-        // $([document.documentElement, document.body]).animate({
-        //   scrollTop: $(`h2:eq(${index})`).offset().top - 90
-        // }, 500)
       }
     }
   }
