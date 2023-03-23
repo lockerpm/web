3c9cd5c07f3990c2889e484aa9b17f5d6bc21af4
duchm <duchm@cystack.net>
2023-03-23 22:27:53 +0700

    (WIP) feat: enter email + otp to view quick share data

--- src/renderer/pages/shares/flash-share-item/_id.vue
+++ src/renderer/pages/shares/flash-share-item/_id.vue
@@ -6,6 +6,26 @@
     </div>
     <!-- Sorry end -->
 
+    <!-- Enter email -->
+    <div v-else-if="requireOtp">
+      <template v-if="!isWaitingOtp">
+        <el-input v-model="email" placeholder="Email">
+          <el-button slot="append" @click="submitEmail">Send OTP</el-button>
+        </el-input>
+      </template>
+
+      <template v-else>
+        <el-input v-model="otp" placeholder="OTP">
+          <el-button slot="append" @click="submitOTP">Get item</el-button>
+        </el-input>
+      </template>
+
+      <p v-if="errorMessage" class="text-danger">
+        {{ errorMessage }}
+      </p>
+    </div>
+    <!-- Enter email end -->
+
     <!-- Cipher detail -->
     <div v-else v-loading="isLoading">
       <DetailQuickSharedCipherContent :cipher-data="send.cipher" />
@@ -49,10 +69,20 @@ export default {
       isLoading: false,
       requireOtp: false,
       isInvalid: false,
+      isWaitingOtp: false,
       send: {
         cipher: {},
         key: null
-      }
+      },
+      email: '',
+      otp: '',
+      errorMessage: ''
+    }
+  },
+
+  computed: {
+    sendId () {
+      return this.$route.params.id
     }
   },
 
@@ -77,6 +107,41 @@ export default {
         this.send.cipher = cipherView
       } catch (error) {
         console.log(error)
+      } finally {
+        this.isLoading = false
+      }
+    },
+
+    async submitEmail () {
+      try {
+        this.isLoading = true
+        await this.$axios.$post(
+          `cystack_platform/pm/quick_shares/${this.sendId}/otp`,
+          { email: this.email }
+        )
+        this.isWaitingOtp = true
+      } catch (error) {
+        console.log(error)
+        this.errorMessage =
+          error.response?.data?.message || error.response?.data?.details
+      } finally {
+        this.isLoading = false
+      }
+    },
+
+    async submitOTP () {
+      try {
+        this.isLoading = true
+        const res = await this.$axios.$post(
+          `cystack_platform/pm/quick_shares/${this.sendId}/public`,
+          { email: this.email, code: this.otp }
+        )
+        this.isWaitingOtp = false
+        this.decryptCipher(res.cipher, this.send.key)
+      } catch (error) {
+        console.log(error)
+        this.errorMessage =
+          error.response?.data?.message || error.response?.data?.details
       } finally {
         this.isLoading = false
       }
