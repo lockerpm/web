4885dfffc2b3eb432635716506722f42f36bd70a
duchm <duchm@MacBook-Pro.local>
2023-03-25 14:40:45 +0700

    style: update lifetime pages styles

--- src/renderer/components/pages/lifetime/RedeemForm.vue
+++ src/renderer/components/pages/lifetime/RedeemForm.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div :class="!needCreateAccount ? 'md:mb-16 md:pb-16' : ''">
     <!-- Need create account? -->
     <el-checkbox v-model="needCreateAccount" class="mb-6">
       {{ $t('lifetime.redeem_page.form.need_create_account') }}
@@ -171,9 +171,14 @@
       @click="handleSubmit"
     >
       <template v-if="isLoading">
-        {{ $t('lifetime.redeem_page.form.submit_btn') }}
+        <span class="font-semibold">
+          {{ $t('lifetime.redeem_page.form.submit_btn') }}
+        </span>
       </template>
-      <span v-else class="flex flex-row justify-center items-center">
+      <span
+        v-else
+        class="flex flex-row justify-center items-center font-semibold"
+      >
         <img
           class="h-6 mr-2.5"
           src="~/assets/images/landing/lifetime/pointer.png"
