558f88c04c86ad3b4b8ab8ae3aac60145bf802b9
quyetnv <quyetnv@cystack.net>
2023-04-05 10:28:38 +0700

    feat: update text

--- src/renderer/components/setting/ChangePassword.vue
+++ src/renderer/components/setting/ChangePassword.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="setting-wrapper">
     <div class="setting-section">
-      <div class="setting-section-header cursor-pointer" @click="collapsed = !collapsed">
+      <div class="setting-section-header" @click="collapsed = !collapsed">
         <div class="text-head-5 font-semibold">
           {{ $t('data.settings.account_password') }}
         </div>
@@ -14,17 +14,8 @@
               <span><i class="fas fa-key mr-2" />{{ $t('data.settings.change_password') }}</span>
             </button>
           </a>
-          <i v-if="!collapsed" class="el-icon-arrow-right" />
-          <i v-else class="el-icon-arrow-down" />
         </div>
       </div>
-    </div>
-    <div v-if="collapsed" class="setting-section">
-      <el-row type="flex">
-        <el-col :lg="12" :sm="16" :xs="24">
-          <p>{{ $t('data.settings.change_password_desc') }}</p>
-        </el-col>
-      </el-row>
     </div>
   </div>
 </template>
