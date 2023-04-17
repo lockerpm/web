65e529fecd72953b2c4eb665345b5043118b1adb
duchm <duchm@MacBook-Pro.local>
2023-04-17 16:43:45 +0700

    chore: update

--- src/renderer/pages/routers/index.vue
+++ src/renderer/pages/routers/index.vue
@@ -16,13 +16,6 @@ export default {
     return {
       CipherType
     }
-  },
-  mounted () {
-    if (this.currentUser.email === 'duchm@cystack.net') {
-      console.log(process.env.nodeEnv)
-      console.log(this.$config.stripeKey)
-      console.log(this.$config.cloudflare)
-    }
   }
 }
 </script>
