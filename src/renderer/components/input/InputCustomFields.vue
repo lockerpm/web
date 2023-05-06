14a446047ef1ef89e90ead98f5269425dc273ea5
quyetnv <quyetnv@cystack.net>
2023-05-06 14:59:12 +0700

    feat: default value

--- src/renderer/components/input/InputCustomFields.vue
+++ src/renderer/components/input/InputCustomFields.vue
@@ -1,19 +1,5 @@
 <template>
   <div>
-    <div class="mb-4">
-      <!-- <el-select
-        v-model="type"
-        class="w-full"
-        @change="(v) => addNewField(v)"
-      >
-        <el-option
-          v-for="option in fieldTypeOptions"
-          :key="option.value"
-          :label="option.label"
-          :value="option.value"
-        />
-      </el-select> -->
-    </div>
     <div v-for="(field, index) in value" :key="index">
       <InputField
         v-model="field.value"
@@ -26,42 +12,10 @@
         @input="(v) => updateField(index, {name: field.name, value: v})"
         @delete="deleteField(index)"
       />
-      <!-- <InputText
-        v-if="field.type !== FieldType.Boolean"
-        v-model="field.value"
-        :label="$t('common.value')"
-        class="w-full"
-        :is-password="field.type === FieldType.Hidden"
-        @input="(v) => updateField(index, {name: field.name, value: v})"
-      /> -->
-      <!-- <InputSelect
-        v-else
-        :label="$t('common.value')"
-        :initial-value="'false'"
-        class="w-full"
-        :options="[
-          {
-            value: 'false',
-            label: 'False'
-          },
-          {
-            value: 'true',
-            label: 'True'
-          }
-        ]"
-        @change="(v) => updateField(index, {name: field.name, value: v})"
-      /> -->
     </div>
 
     <!-- Add new custom field -->
     <div>
-      <!-- <a
-        href="#"
-        class="text-primary cursor-pointer"
-        @click.prevent="addNewField(type, true)"
-      >
-        <i class="el-icon-circle-plus-outline mr-2" />{{ $t('data.ciphers.new_custom_field') }}
-      </a> -->
       <el-dropdown trigger="click">
         <span class="el-dropdown-link text-primary">
           <i class="el-icon-circle-plus-outline mr-2" />{{ $t('data.ciphers.new_custom_field') }}
@@ -71,7 +25,7 @@
             v-for="option in fieldTypeOptions"
             :key="option.value"
             :value="option.value"
-            @click.native="addNewField(option.value)"
+            @click.native="addNewField(option)"
           >
             {{ option.label }}
           </el-dropdown-item>
@@ -85,7 +39,6 @@
 import { FieldView } from '../../jslib/src/models/view'
 import { FieldType } from '../../jslib/src/enums/fieldType'
 import InputField from '../input/InputField.vue'
-// import InputSelect from '../input/InputSelect'
 export default {
   components: {
     InputField
@@ -115,24 +68,17 @@ export default {
           fieldPlaceholder: this.$t('data.ciphers.field_name'),
           placeholder: this.$t('data.ciphers.text')
         },
-        // {
-        //   label: 'Boolean',
-        //   value: FieldType.Boolean
-        // }
         {
           label: 'URL',
           value: 4,
-          placeholder: 'https:// '
+          placeholder: 'https://',
+          defaultValue: 'https://'
         },
         {
           label: 'Email',
           value: 5,
           placeholder: 'xxx@xxxx.xx'
         },
-        // {
-        //   label: this.$t('data.ciphers.address'),
-        //   value: 6
-        // },
         {
           label: this.$t('data.ciphers.date'),
           value: 7,
@@ -154,9 +100,10 @@ export default {
     }
   },
   methods: {
-    addNewField (type) {
+    addNewField (option) {
       const newField = new FieldView()
-      newField.type = type
+      newField.type = option.value
+      newField.value = option.defaultValue || null
       this.$emit('set-fields', { value: newField })
     },
     updateField (index, value) {
