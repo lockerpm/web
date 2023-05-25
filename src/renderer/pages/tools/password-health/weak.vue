cb3ccc54e78f9ad6d16cb488e84134af07958b96
duchm <duchm@MacBook-Pro.local>
2023-05-25 15:35:45 +0700

    chore: count password with no password as weak password

--- src/renderer/pages/tools/password-health/weak.vue
+++ src/renderer/pages/tools/password-health/weak.vue
@@ -19,73 +19,32 @@
       </div>
       <div>
         <span v-if="noData">{{ $t('data.tools.good_news_details') }}</span>
-        <span v-if="haveData">{{ $t('data.tools.weak_passwords_found_details', {data: haveData}) }}</span>
+        <span v-if="haveData">{{
+          $t('data.tools.weak_passwords_found_details', { data: haveData })
+        }}</span>
       </div>
     </div>
     <client-only>
-      <!-- <el-table
-        ref="multipleTable"
-        :data="weakPasswordCiphers || []"
-        style="width: 100%"
-        row-class-name="hover-table-row"
-        :show-header="false"
-      >
-        <el-table-column
-          prop="name"
-          label=""
-        >
-          <template slot-scope="scope">
-            <div class="flex items-center">
-              <div
-                class="text-[34px] mr-3 flex-shrink-0"
-                :class="{'filter grayscale': scope.row.isDeleted}"
-              >
-                <Vnodes :vnodes="getIconCipher(scope.row, 34)" />
-              </div>
-              <div class="flex flex-col">
-                <a
-                  class="text-black font-semibold truncate flex items-center"
-                  :class="{'opacity-80': scope.row.isDeleted}"
-                  @click="routerCipher(scope.row)"
-                >
-                  {{ scope.row.name }}
-                  <img v-if="scope.row.organizationId" src="~/assets/images/icons/shares.svg" alt="Shared" :title="$t('common.shared_with_you')" class="inline-block ml-2">
-                </a>
-                <div>
-                  {{ scope.row.subTitle }}
-                </div>
-              </div>
-            </div>
-          </template>
-        </el-table-column>
-        <el-table-column
-          label=""
-        >
-          <template slot-scope="scope">
-            <PasswordStrength :score="passwordStrengthMap.get(scope.row.id)" />
-          </template>
-        </el-table-column>
-      </el-table> -->
       <RecycleScroller
         ref="list"
         page-mode
         :item-size="65"
         :items="weakPasswordCiphers || []"
       >
-        <template #default="{item}">
+        <template #default="{ item }">
           <div class="td">
             <div class="flex items-center">
               <div
                 class="text-[34px] mr-3 flex-shrink-0"
-                :class="{'filter grayscale': item.isDeleted}"
+                :class="{ 'filter grayscale': item.isDeleted }"
               >
                 <Vnodes :vnodes="getIconCipher(item, 34)" />
               </div>
               <div class="flex flex-col">
                 <a
                   class="text-black font-semibold truncate flex items-center"
-                  :class="{'opacity-80': item.isDeleted}"
-                  @click="routerCipher(item, addEdit)"
+                  :class="{ 'opacity-80': item.isDeleted }"
+                  @click="routerCipher(item)"
                 >
                   {{ item.name }}
                   <img
@@ -116,7 +75,8 @@ import Vnodes from '../../../components/Vnodes'
 import PasswordStrength from '../../../components/password/PasswordStrength'
 export default {
   components: {
-    PasswordStrength, Vnodes
+    PasswordStrength,
+    Vnodes
   },
   props: {
     passwordStrengthMap: {
@@ -140,7 +100,6 @@ export default {
       return this.weakPasswordCiphers && this.weakPasswordCiphers.length === 0
     }
   },
-  mounted () {
-  }
+  mounted () {}
 }
 </script>
