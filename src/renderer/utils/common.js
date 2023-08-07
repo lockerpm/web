8c2e1ba0489b82cbc021c3dae6de9f9543ed3dc7
duchm <duchm@cystack.net>
2023-08-07 13:25:20 +0700

    chore: bump yarn.lock + remove unused code

--- src/renderer/utils/common.js
+++ src/renderer/utils/common.js
@@ -1,5 +1,4 @@
 import creditCardType from 'credit-card-type'
-import parse5 from 'parse5'
 
 export function detectCardBrand (cardNumber) {
   if (!cardNumber) {
@@ -7,44 +6,4 @@ export function detectCardBrand (cardNumber) {
   }
   const card = creditCardType(cardNumber)
   return card[0]?.niceType || null
-}
-
-export function getTagFromHtml (htmlString, tag) {
-  const document = parse5.parse(htmlString)
-
-  function findFirstTag (node) {
-    if (node.tagName === tag) {
-      return node
-    }
-
-    if (node.childNodes) {
-      for (const childNode of node.childNodes) {
-        const n = findFirstTag(childNode)
-        if (n) {
-          return n
-        }
-      }
-    }
-
-    return null
-  }
-
-  return findFirstTag(document)
-}
-
-export function getTextFromPTag (paragraphElement) {
-  let text = ''
-
-  // Traverse all child nodes of the paragraph element
-  for (const node of paragraphElement.childNodes) {
-    if (node.nodeName === '#text') {
-      // For text nodes, add their content to the result
-      text += node.value
-    } else {
-      // For element nodes, recursively get their text content
-      text += getTextFromPTag(node)
-    }
-  }
-
-  return text
 }
