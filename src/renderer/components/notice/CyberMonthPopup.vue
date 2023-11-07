eaf0729a26c30b538c20c0c030afc5ecf24668b6
duchm <duchm@cystack.net>
2023-11-07 18:02:04 +0700

    feat: show promo after unlock

---
+++ src/renderer/components/notice/CyberMonthPopup.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <el-dialog
+      :visible="dialogVisible"
+      width="60%"
+      class="premium-dialog header-no-padding body-no-padding"
+      @close="closeDialog"
+    >
+      <div slot="title">
+        <img
+          width="100%"
+          src="~/assets/images/cyber-month-2023.png"
+          @click="upgradePlan"
+        >
+      </div>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    dialogVisible () {
+      return this.$store.state.notice.showCyberMonthPopup
+    }
+  },
+  methods: {
+    closeDialog () {
+      this.$store.commit('UPDATE_NOTICE', { showCyberMonthPopup: false })
+    },
+    upgradePlan () {
+      this.closeDialog()
+      this.$router.push('/promo/cyber-month-2023')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
