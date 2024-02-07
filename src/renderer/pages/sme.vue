47185c7e7327e5dc520cae116f05a4e4c40e3fe7
duchm <duchm@cystack.net>
2024-02-07 10:18:30 +0700

    (WIP) feat: sme landing page

---
+++ src/renderer/pages/sme.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <!-- Header -->
+    <!-- Header end -->
+
+    <!-- Customers -->
+    <!-- Customers end -->
+
+    <!-- Features -->
+    <!-- Features end -->
+
+    <!-- Hero -->
+    <!-- Hero end -->
+
+    <!-- News -->
+    <!-- News end -->
+
+    <!-- Process -->
+    <!-- Process end -->
+
+    <!-- Why -->
+    <!-- Why end -->
+
+    <!-- Contact -->
+    <!-- Contact end -->
+
+    <!-- Questions -->
+    <!-- Questions end -->
+
+    <!-- CTA -->
+    <!-- CTA end -->
+  </div>
+</template>
+<script>
+export default {
+  layout: 'white'
+}
+</script>
